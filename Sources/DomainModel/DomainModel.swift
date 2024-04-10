7eb2f407b78b38b31c80371bfb8144d5792cc947
Ricwxiao <cxiao4@uw.edu>
2024-04-10 14:00:46 -0700

    finished Money but not able to run tests for Money

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -9,6 +9,51 @@ struct DomainModel {
 // Money
 //
 public struct Money {
+    let amount: Int
+    let currency: String
+    
+    
+    private func toUSD() -> Int {
+        switch currency {
+        case "GBP":
+            return amount*2
+        case "EUR":
+            return Int((Double(amount)/1.5).rounded())
+        case "CAN":
+            return Int((Double(amount)/1.25).rounded())
+        default :
+            return amount
+        }
+    }
+    private func fromUSD(fromAmnt : Int, toCncy: String) -> Int {
+        switch toCncy {
+        case "GBP":
+            return Int((Double(fromAmnt)/2).rounded())
+        case "EUR":
+            return Int((Double(fromAmnt)*1.5).rounded())
+        case "CAN":
+            return Int((Double(fromAmnt)*1.25).rounded())
+        default:
+            return fromAmnt
+        }
+    }
+    
+    func convert(_ toCncy: String) -> Money {
+        let usdAmnt = self.toUSD()
+        let toAmnt = fromUSD(fromAmnt: usdAmnt, toCncy: toCncy)
+        return Money(amount: toAmnt, currency: toCncy)
+    }
+    
+    func add(_ subject : Money) -> Money {
+        let converted = self.convert(subject.currency)
+        let totalAmnt = converted.amount + subject.amount
+        return Money(amount: totalAmnt, currency: subject.currency)
+    }
+    func subtract(_ subject : Money) -> Money {
+        let converted = self.convert(subject.currency)
+        let totalAmnt = converted.amount - subject.amount
+        return Money(amount: totalAmnt, currency: subject.currency)
+    }
 }
 
 ////////////////////////////////////
