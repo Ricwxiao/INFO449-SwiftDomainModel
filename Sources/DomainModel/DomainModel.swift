950b6f7ce1a4d572330c1d938ff911b441628146
Ricwxiao <cxiao4@uw.edu>
2024-04-10 19:55:13 -0700

    passed all tests except for the age limitations

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -38,18 +38,18 @@ public struct Money {
         }
     }
     
-    func convert(_ toCncy: String) -> Money {
+    public func convert(_ toCncy: String) -> Money {
         let usdAmnt = self.toUSD()
         let toAmnt = fromUSD(fromAmnt: usdAmnt, toCncy: toCncy)
         return Money(amount: toAmnt, currency: toCncy)
     }
     
-    func add(_ subject : Money) -> Money {
+    public func add(_ subject: Money) -> Money {
         let converted = self.convert(subject.currency)
         let totalAmnt = converted.amount + subject.amount
         return Money(amount: totalAmnt, currency: subject.currency)
     }
-    func subtract(_ subject : Money) -> Money {
+    public func subtract(_ subject: Money) -> Money {
         let converted = self.convert(subject.currency)
         let totalAmnt = converted.amount - subject.amount
         return Money(amount: totalAmnt, currency: subject.currency)
@@ -64,16 +64,96 @@ public class Job {
         case Hourly(Double)
         case Salary(UInt)
     }
+    
+    var title: String
+    var type: JobType
+    
+    public init(title: String, type: JobType) {
+        self.title = title
+        self.type = type
+    }
+    
+    public func calculateIncome(_ multiplier : Int = 2000) -> Int {
+        switch type {
+        case .Hourly(let wage):
+            return Int(wage * Double(multiplier))
+        case .Salary(let salary):
+            return Int(salary)
+        }
+    }
+    public func raise(byAmount amount: Double) {
+        switch type {
+        case .Salary(let salary):
+            self.type = .Salary(salary + UInt(Int(amount)))
+        case .Hourly(let wage):
+            self.type = .Hourly(wage + amount)
+        }
+    }
+    public func raise(byPercent perc: Double) {
+        switch type {
+        case .Salary(let salary):
+            self.type = .Salary(UInt(Double(salary) + Double(salary) * perc))
+        case .Hourly(let wage) :
+            self.type = .Hourly(wage + wage * Double(perc))
+        }
+    }
 }
 
 ////////////////////////////////////
 // Person
 //
 public class Person {
+    public var firstName: String
+    public var lastName: String
+    public var age: Int
+    public var job: Job?
+    public var spouse: Person?
+    
+    public init(firstName first: String, lastName last: String, age: Int) {
+        self.firstName = first
+        self.lastName = last
+        self.age = age
+        self.job = nil
+        self.spouse = nil
+    }
+    public func toString() -> String {
+        return "[Person: firstName:\(self.firstName) lastName:\(self.lastName) age:\(self.age) job:\(self.job?.title ?? "nil") spouse:\(self.spouse?.firstName ?? "nil")]"
+    }
 }
 
 ////////////////////////////////////
 // Family
 //
 public class Family {
+    var members: [Person]
+    
+    public init(spouse1 one: Person, spouse2 two: Person) {
+        one.spouse = two
+        two.spouse = one
+        self.members = [one, two]
+    }
+    public func haveChild(_ child: Person) -> Bool {
+        var canHave = false
+        for person in members {
+            if person.age >= 21 {
+                canHave = true
+            }
+        }
+        switch canHave {
+        case true:
+            members.append(child)
+        case false:
+            print("None of the members is adult, they cannot have child")
+        }
+        return canHave
+    }
+    public func householdIncome() -> Int {
+        var total = 0
+        for person in members {
+            if person.job != nil {
+                total += person.job!.calculateIncome()
+            }
+        }
+        return total
+    }
 }
